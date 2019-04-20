40196a94e357e8917d496e3d955c5444cd1c0d1c
Raghu Rana <raghurana@gmail.com>
2019-04-20 20:22:49 +1000

    Insert multiple actors is working

---
+++ DvdRentalPostgres.Data/Commands/Actors/InsertActorsCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using DvdRentalPostgres.Data.Entities;
+
+namespace DvdRentalPostgres.Data.Commands.Actors
+{
+    public class InsertActorsCommand : DbOperation<int>
+    {
+        public Actor[] Actors { get; set; }
+
+        public InsertActorsCommand(IDbTransaction transaction) 
+            : base(transaction)
+        {}
+
+        public override Task<int> Execute()
+        {
+            var query = "insert into actor values (@ActorId, @FirstName, @LastName, @LastUpdate)";
+            return Connection.ExecuteAsync(query, Actors, Transaction);
+        }
+    }
+}
