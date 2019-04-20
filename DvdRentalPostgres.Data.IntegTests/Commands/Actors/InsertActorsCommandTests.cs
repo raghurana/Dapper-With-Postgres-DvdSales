7405d89e3f09e75fc68b63c84abdbc9ef6b1dbf4
Raghu Rana <raghurana@gmail.com>
2019-04-20 20:43:18 +1000

    BaseDbCommandTests introduced for automatic rollback of command tests.

---
+++ DvdRentalPostgres.Data.IntegTests/Commands/Actors/InsertActorsCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using DvdRentalPostgres.Data.Commands.Actors;
+using DvdRentalPostgres.Data.Entities;
+using Xunit;
+
+namespace DvdRentalPostgres.Data.IntegTests.Commands.Actors
+{
+    public class InsertActorsCommandTests : BaseDbCommandTests
+    {
+        public InsertActorsCommandTests(DbFixture dbFixture) 
+            : base(dbFixture)
+        {}
+
+        [Fact]
+        public async Task Execute_InsertThreeActors_CountThreeReturned()
+        {
+            var query = new InsertActorsCommand(OpenTransaction)
+            {
+                Actors = new[]
+                {
+                    new Actor {ActorId = 201, FirstName = "Raghu", LastName = "Rana", LastUpdate = DateTime.Now},
+                    new Actor {ActorId = 202, FirstName = "Som", LastName = "Rana", LastUpdate = DateTime.Now},
+                    new Actor {ActorId = 203, FirstName = "Aria", LastName = "Rana", LastUpdate = DateTime.Now}
+                }
+            };
+
+            var insertCount = await query.Execute();
+            Assert.Equal(3, insertCount);
+        }
+    }
+}
---
+++ DvdRentalPostgres.Data.IntegTests/Commands/BaseDbCommandTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using Xunit;
+
+namespace DvdRentalPostgres.Data.IntegTests.Commands
+{
+    [Collection(DbTestsCollection.Name)]
+    public abstract class BaseDbCommandTests : IDisposable
+    {
+        protected IDbConnection Connection;
+        protected IDbTransaction OpenTransaction;
+
+        protected BaseDbCommandTests(DbFixture dbFixture)
+        {
+            Connection = dbFixture.DbConnectionFactory.NewConnection();
+            OpenTransaction = Connection.BeginTransaction();
+        }
+
+        public void Dispose()
+        {
+            OpenTransaction.Rollback();
+            Connection.Dispose();
+            Connection = null;
+        }
+    }
+}
